9af3379b816d15287436e4aa5568cc4e66d6516d
ZleyFerr <zleyferr@bk.ru>
2026-02-18 12:20:57 +0300

    #0 FieldCell class created

---
+++ game15/FieldCell.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+
+class FieldCell
+{
+protected:
+	uint8_t value;
+	uint8_t x_pos;
+	uint8_t y_pos;
+	bool isCellEmpty;
+	bool isUpAviable;
+	bool isDownAviable;
+	bool isLeftAviable;
+	bool isRightAviable;
+public:
+
+
+};
